92edc0be63fecbb2de695b908ac356ea35783dcc
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-07-15 00:10:14 +0530

    Custom Logo

--- functions.php
+++ functions.php
@@ -43,6 +43,13 @@ foreach ( $includes as $i ) {
 /* You can add custom functions below */
 /*-----------------------------------------------------------------------------------*/
 
+// CUSTOM ADMIN LOGIN HEADER LOGO
+ 
+function my_custom_login_logo()
+{
+    echo '<style  type="text/css"> h1 a {  background-image:url(images/logos/yourBoutique.png); } </style>';
+}
+add_action('login_head',  'my_custom_login_logo');
 
 
 
