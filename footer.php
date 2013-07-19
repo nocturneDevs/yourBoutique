3ab3776c1eba84a883f7828776325d202745a6c6
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-07-20 04:07:05 +0530

    Complete rewrite of footer
    New approach treats the site as a white paper placed on top of a black
    chart. The black paper has the same width but is longer than the white
    one. The black part which juts out is the footer. Rewritten from
    scratch so everything has been changed!

--- footer.php
+++ footer.php
@@ -12,28 +12,22 @@
 ?>
 		</div><!-- #main .wrapper -->
 	</div><!-- #page -->
-
 	<footer id="colophon" role="contentinfo">
-	
-		<div id="footerWrapper">
-			<div id="footerNav">
-				
-			</div>
-			<div id="footerDetailText">
-				
-			</div>
-			<div id="footerSocialBar">
-				<i class="icon-facebook icon-3x"></i>
-				<i class="icon-twitter icon-3x"></i>
-				<i class="icon-envelope icon-3x"></i>
-			</div>
-		</div>
-		<div class="site-info">
-			<a href="<?php echo esc_url('http://nocturnedevs.com/'); ?>" title="<?php esc_attr_e( 'Nocturne - Coffee, Cola and Code'); ?>" target="_blank"><?php echo 'Developed by Nocturne'; ?></a>
-		</div><!-- .site-info -->
-	
-	</footer><!-- #colophon -->	
-
+	<div id="footerWrapper">
+				<div id="footerNav">
+				</div>
+				<div id="footerDetailText">
+				</div>
+				<div id="footerSocialBar">
+					<a href="<?php echo esc_url('http://www.facebook.com'); ?>" title="<?php esc_attr_e( 'Like YB on Facebook!'); ?>" target="_blank"><i class="icon-facebook icon-3x"></i></a>
+					<a href="<?php echo esc_url('http://www.twitter.com'); ?>" title="<?php esc_attr_e( 'Follow YB on Twitter!'); ?>" target="_blank"><i class="icon-twitter icon-3x"></i></a>
+					<a href="<?php echo esc_url('http://www.gmail.com'); ?>" title="<?php esc_attr_e( 'Email YB!'); ?>" target="_blank"><i class="icon-envelope icon-3x"></i></a>
+				</div>
+			<div class="site-info">
+				<a href="<?php echo esc_url('http://nocturnedevs.com/'); ?>" title="<?php esc_attr_e( 'Nocturne - Coffee, Cola and Code'); ?>" target="_blank"><?php echo 'Developed by Nocturne'; ?></a>
+			</div><!-- .site-info -->
+	</div><!-- #footerWrapper -->
+	</footer><!-- #colophon -->
 </div><!-- #bodyWrapper -->
 
 <?php wp_footer(); ?>
