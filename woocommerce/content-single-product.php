08440f9a559e558f51f4d2ae56eb2ff5b5394b6d
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-07-20 02:53:27 -0400

    More Product Page Design
    Description now sits beside images.
    Fixed weird sizing issue of "Related Products".
    Adjusted button color.
    Product page is, for the most part, responsive.

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -32,7 +32,7 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 		 */
 		do_action( 'woocommerce_before_single_product_summary' );
 	?>
-	
+
 	<div class="summary entry-summary">
 
 		<?php
