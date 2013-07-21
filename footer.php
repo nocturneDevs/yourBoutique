2064edd76f42fdb1f2505629de6569911ebdb09a
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-07-21 17:23:38 +0530

    Implemented semantic.gs and used it in the footer
    After trying various things, I've realized - semantic.gs is best.
    Period. Super simple. Super elegant.

--- footer.php
+++ footer.php
@@ -15,13 +15,17 @@
 	<footer id="colophon" role="contentinfo">
 	<div id="footerWrapper">
 				<div id="footerNav">
+				Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 				</div>
 				<div id="footerDetailText">
+				Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
 				</div>
-				<div id="footerSocialBar">
-					<div class="socialButton"><a href="<?php echo esc_url('http://www.facebook.com'); ?>" title="<?php esc_attr_e( 'Like YB on Facebook!'); ?>" target="_blank"><i class="icon-facebook icon-3x"></i></a></div>
-					<div class="socialButton"><a href="<?php echo esc_url('http://www.twitter.com'); ?>" title="<?php esc_attr_e( 'Follow YB on Twitter!'); ?>" target="_blank"><i class="icon-twitter icon-3x"></i></a></div>
-					<div class="socialButton"><a href="<?php echo esc_url('http://www.gmail.com'); ?>" title="<?php esc_attr_e( 'Email YB!'); ?>" target="_blank"><i class="icon-envelope icon-3x"></i></a></div>
+				<div id="footerSocial">
+					<div id="footerSocialButtons">
+						<a href="<?php echo esc_url('http://www.facebook.com'); ?>" title="<?php esc_attr_e( 'Like YB on Facebook!'); ?>" target="_blank" class="socialButton"><i class="icon-facebook icon-3x"></i></a>
+						<a href="<?php echo esc_url('http://www.twitter.com'); ?>" title="<?php esc_attr_e( 'Follow YB on Twitter!'); ?>" target="_blank" class="socialButton"><i class="icon-twitter icon-3x"></i></a>
+						<a href="<?php echo esc_url('http://www.gmail.com'); ?>" title="<?php esc_attr_e( 'Email YB!'); ?>" target="_blank" class="socialButton"><i class="icon-envelope icon-3x"></i></a>
+					</div>
 				</div>
 			<div class="site-info">
 				<a href="<?php echo esc_url('http://nocturnedevs.com/'); ?>" title="<?php esc_attr_e( 'Nocturne - Coffee, Cola and Code'); ?>" target="_blank"><?php echo 'Developed by Nocturne'; ?></a>
