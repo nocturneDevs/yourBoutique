eb54cc3534450e4e22261292fa30884c2ce760da
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-07-24 17:04:38 -0400

    Started home page

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The default template for displaying content. Used for both single and index/archive/search.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header('shop'); ?>
+
+
+
+
+
+<?php get_footer(); ?>
+
