d3a770803e3f5bb69413e99af74d578311d53d57
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-07-24 18:00:33 -0400

    Primitive Home Page in place
    Currently, home page presents results of 3 queries
    - Clothing
    - Bags
    - Accessories
    
    Should be trivial to extend this concept to new user-definable
    categories such as "sale", "featured", etc.

--- home.php
+++ home.php
@@ -10,6 +10,200 @@
 get_header('shop'); ?>
 
 
+<h1 class="page-title">Clothing</h1>
+
+<?php 
+
+$args = array(
+                'post_type' => 'product',
+                'posts_per_page' => 8,
+                'tax_query' => array (
+                	array (
+                		'taxonomy' => 'product_cat',
+                		'field' => 'slug',
+                		'terms' => 'clothing'
+                	)
+                )
+            );
+
+$wp_query = new WP_Query( $args );
+
+?>
+
+<?php do_action( 'woocommerce_archive_description' ); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook
+				 *
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php woocommerce_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+
+			<?php
+				/**
+				 * woocommerce_after_shop_loop hook
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
+
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+			<?php woocommerce_get_template( 'loop/no-products-found.php' ); ?>
+
+		<?php endif; ?>
+
+
+
+
+
+
+<h1 class="page-title">Bags</h1>
+
+<?php 
+
+$args = array(
+                'post_type' => 'product',
+                'posts_per_page' => 8,
+                'tax_query' => array (
+                	array (
+                		'taxonomy' => 'product_cat',
+                		'field' => 'slug',
+                		'terms' => 'bags'
+                	)
+                )
+            );
+
+$wp_query = new WP_Query( $args );
+
+?>
+
+<?php do_action( 'woocommerce_archive_description' ); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook
+				 *
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php woocommerce_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+
+			<?php
+				/**
+				 * woocommerce_after_shop_loop hook
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
+
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+			<?php woocommerce_get_template( 'loop/no-products-found.php' ); ?>
+
+		<?php endif; ?>
+
+
+
+
+
+
+
+
+<h1 class="page-title">Accessories</h1>
+
+<?php 
+
+$args = array(
+                'post_type' => 'product',
+                'posts_per_page' => 8,
+                'tax_query' => array (
+                	array (
+                		'taxonomy' => 'product_cat',
+                		'field' => 'slug',
+                		'terms' => 'accessories'
+                	)
+                )
+            );
+
+$wp_query = new WP_Query( $args );
+
+?>
+
+<?php do_action( 'woocommerce_archive_description' ); ?>
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook
+				 *
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php woocommerce_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+
+			<?php
+				/**
+				 * woocommerce_after_shop_loop hook
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
+
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+			<?php woocommerce_get_template( 'loop/no-products-found.php' ); ?>
+
+		<?php endif; ?>
 
 
 
