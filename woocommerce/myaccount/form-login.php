29bec7d7893db80341466f0db5a5734de38c1e83
Sanjay Guruprasad <sanjay@nocturnedevs.com>
2013-07-26 01:16:23 +0530

    Login Page - minimalist page designed
    Redesigned from scratch. Grid based.
    GLOBAL CSS CHANGE: Border-boxed everything

--- woocommerce/myaccount/form-login.php
+++ woocommerce/myaccount/form-login.php
@@ -26,16 +26,16 @@ global $woocommerce; ?>
 		<h2><?php _e( 'Login', 'woocommerce' ); ?></h2>
 		<form method="post" class="login">
 			<p class="form-row form-row-first">
-				<label for="username"><?php _e( 'Username or email', 'woocommerce' ); ?> <span class="required">*</span></label>
-				<input type="text" class="input-text" name="username" id="username" />
+				<label for="username"><?php _e( 'Email', 'woocommerce' ); ?> <span class="required">*</span></label>
+				<input type="email" class="input-text" name="username" id="username" autofocus="TRUE" placeholder="email@domain.com" />
 			</p>
 			<p class="form-row form-row-last">
 				<label for="password"><?php _e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
-				<input class="input-text" type="password" name="password" id="password" />
+				<input class="input-text" type="password" name="password" id="password" placeholder="xyz" />
 			</p>
 			<div class="clear"></div>
 
-			<p class="form-row">
+			<p class="form-row buttonRow">
 				<?php $woocommerce->nonce_field('login', 'login') ?>
 				<input type="submit" class="button" name="login" value="<?php _e( 'Login', 'woocommerce' ); ?>" />
 				<a class="lost_password" href="<?php
@@ -58,13 +58,13 @@ global $woocommerce; ?>
 	<div class="col-2">
 
 		<h2><?php _e( 'Register', 'woocommerce' ); ?></h2>
-		<form method="post" class="register">
+		<form method="post" class="register"  autocomplete="off">
 
 			<?php if ( get_option( 'woocommerce_registration_email_for_username' ) == 'no' ) : ?>
 
 				<p class="form-row form-row-first">
 					<label for="reg_username"><?php _e( 'Username', 'woocommerce' ); ?> <span class="required">*</span></label>
-					<input type="text" class="input-text" name="username" id="reg_username" value="<?php if (isset($_POST['username'])) echo esc_attr($_POST['username']); ?>" />
+					<input type="text" class="input-text" name="username" id="reg_username" placeholder="pikachu" value="<?php if (isset($_POST['username'])) echo esc_attr($_POST['username']); ?>" />
 				</p>
 
 				<p class="form-row form-row-last">
@@ -76,18 +76,18 @@ global $woocommerce; ?>
 			<?php endif; ?>
 
 				<label for="reg_email"><?php _e( 'Email', 'woocommerce' ); ?> <span class="required">*</span></label>
-				<input type="email" class="input-text" name="email" id="reg_email" value="<?php if (isset($_POST['email'])) echo esc_attr($_POST['email']); ?>" />
+				<input type="email" class="input-text" name="email" id="reg_email" placeholder="email@domain.com" value="<?php if (isset($_POST['email'])) echo esc_attr($_POST['email']); ?>" />
 			</p>
 
 			<div class="clear"></div>
 
 			<p class="form-row form-row-first">
 				<label for="reg_password"><?php _e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
-				<input type="password" class="input-text" name="password" id="reg_password" value="<?php if (isset($_POST['password'])) echo esc_attr($_POST['password']); ?>" />
+				<input type="password" class="input-text" name="password" id="reg_password" placeholder="xyz" value="<?php if (isset($_POST['password'])) echo esc_attr($_POST['password']); ?>" />
 			</p>
 			<p class="form-row form-row-last">
 				<label for="reg_password2"><?php _e( 'Re-enter password', 'woocommerce' ); ?> <span class="required">*</span></label>
-				<input type="password" class="input-text" name="password2" id="reg_password2" value="<?php if (isset($_POST['password2'])) echo esc_attr($_POST['password2']); ?>" />
+				<input type="password" class="input-text" name="password2" id="reg_password2" placeholder="and again" value="<?php if (isset($_POST['password2'])) echo esc_attr($_POST['password2']); ?>" />
 			</p>
 			<div class="clear"></div>
 
@@ -96,7 +96,7 @@ global $woocommerce; ?>
 
 			<?php do_action( 'register_form' ); ?>
 
-			<p class="form-row">
+			<p class="form-row buttonRow">
 				<?php $woocommerce->nonce_field('register', 'register') ?>
 				<input type="submit" class="button" name="register" value="<?php _e( 'Register', 'woocommerce' ); ?>" />
 			</p>
