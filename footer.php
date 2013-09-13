bc50f16b584600e312569d7aef17e2d541a1bf78
Hariharan Mohanraj <hari.mohanraj89@gmail.com>
2013-09-13 00:53:19 -0400

    heading font size increase, footer link mod

--- footer.php
+++ footer.php
@@ -45,10 +45,10 @@
 				</div>
 				<div id="footerSocial">
 					<div id="footerSocialButtons">
-						<a href="<?php echo esc_url('https://www.facebook.com/pages/YourBoutique/400076350083620'); ?>" title="<?php esc_attr_e( 'Like YB on Facebook!'); ?>" target="_blank" class="socialButton" id="facebookSocial"><i class="icon-facebook icon-3x"></i></a>
+						<a href="<?php echo esc_url('http://www.facebook.com/pages/YourBoutique/400076350083620'); ?>" title="<?php esc_attr_e( 'Like YB on Facebook!'); ?>" target="_blank" class="socialButton" id="facebookSocial"><i class="icon-facebook icon-3x"></i></a>
 						<a href="<?php echo esc_url('http://www.twitter.com'); ?>" title="<?php esc_attr_e( 'Follow YB on Twitter!'); ?>" target="_blank" class="socialButton" id="twitterSocial"><i class="icon-twitter icon-3x"></i></a>
-						<a href="<?php echo esc_url('http://www.gmail.com'); ?>" title="<?php esc_attr_e( 'Email YB!'); ?>" target="_blank" class="socialButton" id="mailSocial"><i class="icon-envelope icon-3x"></i></a>
-						<a href="<?php echo esc_url('https://pinterest.com'); ?>" title="<?php esc_attr_e( 'Check out YB on Pinterest!'); ?>" target="_blank" class="socialButton" id="pinterestSocial"><i class="icon-pinterest icon-3x"></i></a>
+						<a href="<?php echo esc_url('mailto:yourboutiquefb@gmail.com'); ?>" title="<?php esc_attr_e( 'Email YB!'); ?>" target="_blank" class="socialButton" id="mailSocial"><i class="icon-envelope icon-3x"></i></a>
+						<a href="<?php echo esc_url('http://www.pinterest.com'); ?>" title="<?php esc_attr_e( 'Check out YB on Pinterest!'); ?>" target="_blank" class="socialButton" id="pinterestSocial"><i class="icon-pinterest icon-3x"></i></a>
 						<a href="<?php echo esc_url('http://www.youtube.com'); ?>" title="<?php esc_attr_e( 'Subscribe to YB on YouTube!'); ?>" target="_blank" class="socialButton" id="youtubeSocial"><i class="icon-youtube icon-3x"></i></a>
 					</div>
 				</div>
